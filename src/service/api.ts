21cf469d4a10f95c1d2f0fe8ea2eb300dd0863b1
SamirBatista <samir_viana1@hotmail.com>
2023-02-16 22:10:08 -0300

    editUrl

--- src/service/api.ts
+++ src/service/api.ts
@@ -7,7 +7,7 @@ class Api {
 
     setBaseURL() {
         return axios.create({
-            baseURL: 'https://api-recados-growdev.vercel.app',
+            baseURL: 'https://api-fawn-xi.vercel.app',
         });
     }
 
