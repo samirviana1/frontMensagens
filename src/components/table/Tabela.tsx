d9453704b9351e164c60024ac4557b8dfbd3220d
SamirBatista <samir_viana1@hotmail.com>
2023-02-23 19:44:42 -0300

    logTable

--- src/components/table/Tabela.tsx
+++ src/components/table/Tabela.tsx
@@ -36,6 +36,7 @@ export default function Tabela() {
         (i) => i.uid === usuarioOn?.id
       );
       setRow(minhaMensagens);
+      console.log("effect", minhaMensagens);
     }
   }, [listaMensagem]);
   return (
