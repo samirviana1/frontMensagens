a0c1fe2f0bce6b4da5c8bf23226fe4ec5383c12f
SamirBatista <samir_viana1@hotmail.com>
2023-02-25 21:33:26 -0300

    home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -38,7 +38,7 @@ function Home() {
 
   useEffect(() => {
     dispacth(getIdStickynotes());
-  }, []);
+  }, [noteUserLogado]);
 
   const [descricao, setDescricao] = useState("");
   const [detalhamento, setDetalhamento] = useState("");
