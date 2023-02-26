061c977d802f76bbe65c0bc40ca8d81a0a04f5cb
SamirBatista <samir_viana1@hotmail.com>
2023-02-26 16:51:32 -0300

    teste

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -42,7 +42,7 @@ function Home() {
 
   useEffect(() => {
     dispacth(getall);
-  }, []);
+  }, [getall]);
 
   const [descricao, setDescricao] = useState("");
   const [detalhamento, setDetalhamento] = useState("");
